68ea57701785f847a9280fa77c5b8ebe8e0be2f4
Utopia-015 <42064997+Utopia-015@users.noreply.github.com>
2022-03-19 16:20:27 +0800

    Create execbot.sh

---
+++ execbot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pgrep node > /dev/null
+if [ $? -ne 0 ]
+then 
+	cd /root/gocq
+  ./go-cqhttp faststart > ./logs/mylog.log 2>&1 &
+  sleep 3
+  cd /root/cq-picsearcher-bot
+  npm start
+else
+  cd /root/cq-picsearcher-bot
+  npm stop
+	pkill go-cqhttp
+fi
